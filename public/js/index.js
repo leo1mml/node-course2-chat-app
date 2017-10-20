881be22f5a9b47ae664c64ff36558ac436e99554
Leonel <mml_leo@hotmail.com>
2017-10-20 00:51:15 -0200

    adding broadcast message and self message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,11 @@ var socket = io();
 
 socket.on('connect', function() {
    console.log('Connected to server');
-   
 })
 socket.on('disconnect', function() {
     console.log('disconnected to server');
 })
+
 
 socket.on('newMessage', function(message) {
     console.log('newMessage', message);
